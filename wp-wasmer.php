a54328c9650691302329b3ad401d88c4952bc18e
Syrus Akbary <me@syrusakbary.com>
2025-05-16 11:00:09 +0200

    Updated Wasmer plugin version

--- wp-wasmer.php
+++ wp-wasmer.php
@@ -6,7 +6,7 @@
  * Description: Wasmer Plugin for WordPress
  * Author: Wasmer
  * Author URI: https://wasmer.io
- * Version: 0.1.5
+ * Version: 0.1.6
  * Text Domain: wasmer
  * Domain Path: /languages/
  * Requires at least: 6.0
@@ -18,10 +18,10 @@
  * @package  wp-wasmer
  * @category Core
  * @author   Wasmer
- * @version  0.1.5
+ * @version  0.1.6
  */
 
-define( 'WP_WASMER_PLUGIN_VERSION', '0.1.5' );
+define( 'WP_WASMER_PLUGIN_VERSION', '0.1.6' );
 
 if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
